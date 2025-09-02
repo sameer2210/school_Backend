023618b2ab4207d5e0a7f3401e71834022e172cd
SAM <sameerkhan27560@gmail.com>
2025-09-02 18:25:06 +0530

    vercel frontend

--- src/app.js
+++ src/app.js
@@ -1,17 +1,35 @@
-import cors from 'cors';                                  //allow request from other domains
+import cors from 'cors'; //allow request from other domains
+import dotenv from 'dotenv';
 import express from 'express';
-import helmet from 'helmet';                              //Adds security headers
-import morgan from 'morgan';
 import rateLimit from 'express-rate-limit';
+import helmet from 'helmet'; //Adds security headers
+import morgan from 'morgan';
 import schoolRoutes from './routes/school.routes.js';
+dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [
+  'http://localhost:3000',
+  'https://school-frontend-ashy.vercel.app',
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not alllow by cors'));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(helmet());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(rateLimit({ windowMs: 60_000, max: 120 }));           //request - limit max 120/min to protect from brute force
+app.use(rateLimit({ windowMs: 60_000, max: 120 })); //request - limit max 120/min to protect from brute force
 
 app.get('/', (req, res) => res.json({ success: true, message: 'API is running' }));
 app.use('/', schoolRoutes);
